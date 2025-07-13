03de6b3d3eb0988d86845eb3c214f49eaee0046e
onovich <hjn1110@163.com>
2025-07-14 03:38:21 +0800

    <feature> add: Drag

--- Assets/Scripts_Runtime/View/CardView.cs
+++ Assets/Scripts_Runtime/View/CardView.cs
@@ -1,10 +1,9 @@
-using System;
-using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
-public class CardView : MonoBehaviour {
-
+public class CardView : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
     [SerializeField] RectTransform trans;
     [SerializeField] Button button;
     [SerializeField] Image suitImage;
@@ -16,19 +15,31 @@ public class CardView : MonoBehaviour {
 
     int cardId;
     public int CardId => cardId;
+    bool isDragging;
+    Vector2 offset;
+    Canvas canvas;
 
-    Action<int> OnClickHandler;
+    public System.Action<int> OnClickHandler;
+    public System.Action<int> OnBeginDragEvent;
+    public System.Action<int> OnDragEvent;
+    public System.Action<int> OnEndDragEvent;
 
-    public void Ctor(int id, Sprite suitSprite, Color suitColor, string randString) {
-        cardId = id;
+    void Awake() {
+        canvas = GetComponentInParent<Canvas>();
         button.onClick.AddListener(() => {
-            OnClickHandler?.Invoke(cardId);
+            if (!isDragging) {
+                OnClickHandler?.Invoke(cardId);
+            }
         });
+    }
+
+    public void Ctor(int id, Sprite suitSprite, Color suitColor, string rankString) {
+        cardId = id;
         suitImage.sprite = suitSprite;
         suitImage.color = suitColor;
         miniSuitImage.sprite = suitSprite;
         miniSuitImage.color = suitColor;
-        rankText.text = randString;
+        rankText.text = rankString;
         rankText.color = suitColor;
     }
 
@@ -36,4 +47,34 @@ public class CardView : MonoBehaviour {
         trans.position = pos;
     }
 
+    public void OnBeginDrag(PointerEventData eventData) {
+        isDragging = true;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.transform as RectTransform,
+            eventData.position,
+            eventData.pressEventCamera,
+            out offset);
+        offset -= new Vector2(trans.localPosition.x, trans.localPosition.y);
+
+        OnBeginDragEvent?.Invoke(cardId);
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        Vector2 localPointerPosition;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.transform as RectTransform,
+            eventData.position,
+            eventData.pressEventCamera,
+            out localPointerPosition)) {
+            trans.localPosition = localPointerPosition - offset;
+        }
+
+        OnDragEvent?.Invoke(cardId);
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        isDragging = false;
+        OnEndDragEvent?.Invoke(cardId);
+    }
 }
